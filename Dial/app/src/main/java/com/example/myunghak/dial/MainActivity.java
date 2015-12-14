16739eacffc11ed63c89131ceae2e3d0b6e71584
myunghakLee <crying_crane@naver.com>
2015-12-14 15:46:19 +0900

    Dial finish

--- Dial/app/src/main/java/com/example/myunghak/dial/MainActivity.java
+++ Dial/app/src/main/java/com/example/myunghak/dial/MainActivity.java
@@ -2,29 +2,98 @@ package com.example.myunghak.dial;
 
 import android.app.Activity;
 import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
-public class IntentDemo1B extends Activity {
-    EditText txtUriString;
-    Button btnCallActivity2;
-    Context context = getApplication();
+public class MainActivity extends Activity {
 
-    @Override
+    final Context context = this;
+    private Button button;
+
+    EditText Pnum;
 
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        try {
-            txtUriString = (EditText) findViewById(R.id.txtUriString);
-            btnCallActivity2 = (Button) findViewById(R.id.btnCallActivity2);
-            btnCallActivity2.setOnClickListener(new MyClickHandler());
-        } catch (Exception e) {
-            Toast.makeText(context, e.getMessage(), Toast.LENGTH_LONG).show();
+        setContentView(R.layout.activity_main);
+
+
+
+        button = (Button) findViewById(R.id.buttonCall);
+        Pnum = (EditText)findViewById(R.id.PhoneNumber);
+        // add PhoneStateListener
+        PhoneCallListener phoneListener = new PhoneCallListener();
+        TelephonyManager telephonyManager = (TelephonyManager) this
+                .getSystemService(Context.TELEPHONY_SERVICE);
+        telephonyManager.listen(phoneListener,PhoneStateListener.LISTEN_CALL_STATE);
+
+        // add button listener
+        button.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                String PhoneNumber = Pnum.getText().toString();
+
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:" + PhoneNumber));
+
+                startActivity(callIntent);
+            }
+        });
+
+    }
+
+    //monitor phone call activities
+    private class PhoneCallListener extends PhoneStateListener {
+
+        private boolean isPhoneCalling = false;
+
+        String LOG_TAG = "LOGGING 123";
+
+        @Override
+        public void onCallStateChanged(int state, String incomingNumber) {
+
+            if (TelephonyManager.CALL_STATE_RINGING == state) {
+                // phone ringing
+                Log.i(LOG_TAG, "RINGING, number: " + incomingNumber);
+            }
+
+            if (TelephonyManager.CALL_STATE_OFFHOOK == state) {
+                // active
+                Log.i(LOG_TAG, "OFFHOOK");
+
+                isPhoneCalling = true;
+            }
+
+            if (TelephonyManager.CALL_STATE_IDLE == state) {
+                // run when class initial and phone call ended,
+                // need detect flag from CALL_STATE_OFFHOOK
+                Log.i(LOG_TAG, "IDLE");
+
+                if (isPhoneCalling) {
+
+                    Log.i(LOG_TAG, "restart app");
+
+                    // restart app
+                    Intent i = getBaseContext().getPackageManager()
+                            .getLaunchIntentForPackage(
+                                    getBaseContext().getPackageName());
+                    i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(i);
+
+                    isPhoneCalling = false;
+                }
+
+            }
         }
-    }// onCreate
+    }
 
 }
